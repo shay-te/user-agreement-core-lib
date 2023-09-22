3927bfdd7929725ef534e8d16577fd49cbb67ea3
shay.te@gmail.com <shay.te@gmail.com>
2023-09-22 20:54:26 +0300

    update check for duplicate

--- user_agreement_core_lib/data_layers/service/agreement_document_service.py
+++ user_agreement_core_lib/data_layers/service/agreement_document_service.py
@@ -1,3 +1,5 @@
+from sqlalchemy.exc import IntegrityError
+
 from core_lib.data_layers.service.service import Service
 from core_lib.data_transform.result_to_dict import result_to_dict, ResultToDict
 from user_agreement_core_lib.data_layers.data_access.agreement_document_data_access import AgreementDocumentDataAccess
@@ -18,7 +20,13 @@ class AgreementDocumentService(Service):
     @ResultToDict()
     def agree(self, user_id: int, document_id: int):
         assert self._agreement_document.get(document_id)  # raise 404 when not found
-        return self._user_agreement_document_da.agree(user_id, document_id)
+        try:
+            return self._user_agreement_document_da.agree(user_id, document_id)
+        except IntegrityError as ex:
+            if ex and ex.orig and ex.orig.pgcode == '23505':
+                # this agreement already been agreed to
+                return
+            raise ex
 
     @ResultToDict()
     def disagree(self, user_id: int, document_id: int):
