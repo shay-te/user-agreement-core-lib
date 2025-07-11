5cc68f8e6721d980a4eecf2181124dea928914af
oren golan <oren@getuna.ai>
2025-07-11 11:07:18 +0300

    update is_agreed_by_name is working

--- user_agreement_core_lib/data_layers/service/agreement_document_service.py
+++ user_agreement_core_lib/data_layers/service/agreement_document_service.py
@@ -1,5 +1,6 @@
 from core_lib.data_layers.service.service import Service
 from core_lib.data_transform.result_to_dict import ResultToDict
+from user_agreement_core_lib.data_layers.data.agreement_db.entities.user_agreement_document import UserAgreementDocument
 from user_agreement_core_lib.data_layers.data_access.agreement_document_data_access import AgreementDocumentDataAccess
 from user_agreement_core_lib.data_layers.data_access.user_agreement_document_data_access import (
     UserAgreementDocumentDataAccess,
@@ -48,4 +49,5 @@ class AgreementDocumentService(Service):
         return self._agreement_document.get_latest_version(name, language)
 
     def is_agreed_by_name(self, user_id: int, document_name: str, language: str) -> bool:
-        return True if self._user_agreement_document_da.get_agreed_document_by_name(user_id, document_name, language) else False
+        user_agreement_doc = self._user_agreement_document_da.get_agreed_document_by_name(user_id, document_name, language)
+        return bool(user_agreement_doc and user_agreement_doc.is_agreed)
