bcd6e039a8c4e2a63565b21159b9d314eec8006b
oren golan <oren@getuna.ai>
2025-07-11 15:17:10 +0300

    updates after done tests

--- user_agreement_core_lib/data_layers/service/agreement_document_service.py
+++ user_agreement_core_lib/data_layers/service/agreement_document_service.py
@@ -17,32 +17,28 @@ class AgreementDocumentService(Service):
         self._user_agreement_document_da = user_agreement_document_da
 
     @ResultToDict()
-    def agree(self, user_id: int, document_name: str, language: str, document_id: int):
-        assert self._agreement_document.get(document_id)  # raise 404 when not found
-
+    def agree(self, user_id: int, document_name: str, language: str):
         existing = self._user_agreement_document_da.get_agreed_document_by_name(user_id=user_id,document_name= document_name, language=language)
+        assert existing
 
-        if existing and existing.is_agreed:
+        if existing.is_agreed:
             return existing
 
-        if existing:
-            self._user_agreement_document_da.delete(user_id, document_id)
-
-        return self._user_agreement_document_da.set_agreement(user_id, document_id, True)
+        doc_id = existing.id
+        self._user_agreement_document_da.delete(user_id, doc_id)
+        return self._user_agreement_document_da.set_agreement(user_id, doc_id, True)
 
     @ResultToDict()
-    def disagree(self, user_id: int, document_name: str, language: str, document_id: int):
-        assert self._agreement_document.get(document_id)  # raise 404 when not found
-
+    def disagree(self, user_id: int, document_name: str, language: str):
         existing = self._user_agreement_document_da.get_agreed_document_by_name(user_id=user_id, document_name=document_name, language=language)
+        assert existing
 
-        if existing and not existing.is_agreed:
+        if not existing.is_agreed:
             return existing
 
-        if existing:
-            self._user_agreement_document_da.delete(user_id, document_id)
-
-        return self._user_agreement_document_da.set_agreement(user_id, document_id, False)
+        doc_id = existing.id
+        self._user_agreement_document_da.delete(user_id, doc_id)
+        return self._user_agreement_document_da.set_agreement(user_id, doc_id, False)
 
     @ResultToDict()
     def get_document_latest_version(self, name: str, language: str):
